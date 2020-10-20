5ad22c54329178b92e52c0d5d35f85e3cc19d4bf
17rey17 <rey4pk@gmail.com>
2020-10-19 17:52:22 -0700

    Update

--- my-beats/src/components/Registration.js
+++ my-beats/src/components/Registration.js
@@ -1,33 +1,36 @@
 import React from 'react'
-import { Form } from "react-bootstrap";
 
-const Registration = () => 
-{
-   
-    return(
-        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+const Registration = () => {
+
+    return (
+
+        <div>
+
+            <h1>Start seeing your Beats.</h1>
+            <h6>Create your account below</h6>
+
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '75vh' }}>
+                <form>
+                    <label htmlFor="uname">Username:</label><br />
+                    <input type="text" id="uname" name="uname" /><br />
+                    <label htmlFor="password">Password:</label><br />
+                    <input type="text" id="password" name="password" /><br />
+                    <label htmlFor="email">Email:</label><br />
+                    <input type="text" id="email" name="email" />
+                    <br />
+                    <input type="submit" value="Register" className="btn btn-primary btn-b"></input>
+                </form>
+            </div>
 
 
-            <Form>
-                <div className='form-group'>
-                    <label htmlFor='name'>Name</label>
-                    <input type='text'/>
-                </div>
-                <div className='form-group'>
-                    <label htmlFor='email'>Email</label>
-                    <input type='email' />
-                </div>
-                <div className='form-group'>
-                    <label htmlFor='password'>Password</label>
-                    <input type='password' />
-                </div>
-                <div className='form-group'>
-                    <label htmlFor='passConfirm'>Confirm Password</label>
-                    <input type='password' />
-                </div>
-                
-                <input type="submit" value="Register" className="btn btn-primary btn-b"></input>
-            </Form>
+
+
+
+
+
+
+
+
 
         </div>
 
