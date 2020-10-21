706d3f99ca0a43b71d30656f82a9f680f9d12871
Alex Burke <aburke07@calpoly.edu>
2020-10-21 08:33:18 -0700

    Update CreateAnAccount.js

--- my-beats/src/components/CreateAnAccount.js
+++ my-beats/src/components/CreateAnAccount.js
@@ -4,25 +4,10 @@ import Registration from './Registration';
 
 const CreateAnAccount = () => {
     return (
-      <div className="create-account">
-        <h1>Start seeing your Beats.</h1>
-        <h5>Create your account below</h5>
-        <form name="create-account-form">
-          <label for="uname">Username:</label><br />
-          <input type="text" id="uname" name="uname" /><br />
-          <label for="password">Password:</label><br />
-          <input type="text" id="password" name="password" /><br />
-          <label for="email">Email:</label><br />
-          <input type="text" id="email" name="email" />
-          <br />
-          <input type="button" onClick="submitForm()" value="Create Account"/>
-        </form>
-      </div>
       <div>
-    
-            <Registration />
-      </div>        
-     
+         <Registration />
+      </div>
+
     );
 }
 
