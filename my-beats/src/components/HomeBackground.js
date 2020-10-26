21fe7c52d1cc5445413b2243f94c94c0b00ce3cd
Alex Burke <aburke07@calpoly.edu>
2020-10-25 20:38:19 -0700

    responsive homepage fix + footer start

--- my-beats/src/components/HomeBackground.js
+++ my-beats/src/components/HomeBackground.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const HomeBackGround = () =>{
-    
+
     return (
         <div className='hero-container' style={{flexDirection:'column',alignItems:'center',display:'flex', objectFit:'contain'}} >
-            <video src='/Videos/Music.mp4' autoPlay loop muted style={{objectFit: 'cover', width: '100%', height: '100', position: 'fixed', zIndex:'-1'}}></video>
+            <video src='/Videos/Music.mp4' autoPlay loop muted style={{objectFit: 'cover', width: '101%', height: '100%', position: 'fixed', zIndex:'-1'}}></video>
             <h1 style={{textAlign:'center', color:'#F6F4E6', fontSize:'70px', marginTop:'10%'}}>Welcome to MyBeats!</h1>
             <p style={{textAlign:'center', color: '#F6F4E6', padding: '2px', width: '460px', fontSize: '15px', background: '#41444B', opacity: '0.9'}}>
                 Music is more than just what flows through your ears. Music is a full
@@ -13,14 +13,12 @@ const HomeBackGround = () =>{
                do one deeper. MyBeats gives you the power to understand your music.
                Stop listening, start seeing.</p>
 
-                
+
             </div>
     )
 }
-        
-        
-        
+
+
+
 
 export default HomeBackGround
-
-
