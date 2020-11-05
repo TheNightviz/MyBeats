be0ce196796aed604d24ff7f7529c52c115b9fbe
Alex Burke <aburke07@calpoly.edu>
2020-11-05 10:06:17 -0800

    various landing page updates

--- my-beats/src/components/BottomFooter.js
+++ my-beats/src/components/BottomFooter.js
@@ -1,25 +1,32 @@
 import React from 'react'
 
+var phantom = {
+  display: 'block',
+  padding: '20px',
+  height: '30px',
+  width: '100%',
+}
+
 const BottomFooter = () => {
     return (
-      <div className="bottom-footer"
+      <div className="fixed-bottom"
          style={{justifyContent:'center',
             background: 'none repeat scroll 0 0 grey',
             opacity: '50%',
-            bottom: '0',
             position: 'fixed',
+            bottom: '0px',
             alignItems: 'center',
             width: '100%'
             }}>
-      <h6
-         style={{background: 'none repear scroll 0 0 transparent',
-         height: '30px',
-         padding: '7px',
-         margin: 'auto',
-         opacity: '100%',
-         width: '245px'}}>Copyright &copy; 2020 BoomerBeats</h6>
+        <h6
+          style={{background: 'transparent',
+          padding: '7px',
+          margin: 'auto',
+          opacity: '100%',
+          width: '245px'}}>Copyright &copy; 2020 BoomerBeats</h6>
       </div>
 
     );
 }
+
 export default BottomFooter;
