6190eaffadbf43eb299f500757b6c3ef6c9c38db
Simon Ibssa <ibssasimon@gmail.com>
2020-11-12 18:46:32 -0800

    adding api endpoints

--- my-beats/src/userAuth.js
+++ my-beats/src/userAuth.js
@@ -8,36 +8,8 @@
 
 */
 
-
-/** 
+ 
 var express = require('express'); // Express web server framework
-var request = require('request'); // "Request" library
-var cors = require('cors');
-var querystring = require('querystring');
-var cookieParser = require('cookie-parser');
-
-
-var client_id = 'f8b0b0aa401a4597af0caa66ac4bb9e5';
-var client_secret = '0e447f9e9629472ea35eb73a13f45a1f';
-var redirect_uri = 'http://localhost:8888/callback';
-*/
-
-
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
-
-
-
-/*export function getLogin() {
-  userAuth.get('/login', function(req, res) {
-    var scopes = 'user-read-private user-read-email';
-    res.redirect('https://accounts.spotify.com/authorize' +
-      '?response_type=code' +
-      '&client_id=' + client_id +
-      (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-      '&redirect_uri=' + encodeURIComponent(redirect_uri));
-    });
-}*/
+var request = require('request'); // "Request" librarry
+var user_id = "ibssasimon";
+var token = "Bearer BQC2xiYZfhbRIE59gFDzdKvMzLlZNSVs_uihwjzW83ZnDI4U-N4isAO_R007_hIxBAqFb9sQKkunZtqcEIftsjpLf3VohFhjBywKZKAqMqTp17QJGUTfefBfm6VeLV1DI9iWdzGE_U6lNK8YBQ";
