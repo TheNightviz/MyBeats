1d840cfa606a586b636c42517251ff9dcf5a3e89
17rey17 <rey4pk@gmail.com>
2020-11-13 11:27:30 -0800

    User pages

---
+++ my-beats/src/components/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserNav from './UserNav';
+
+const MainPage = () =>
+{
+       return( <div>
+           <UserNav />
+
+           <h1>test</h1>  
+
+       </div>)
+      
+}
+
+export default MainPage;
