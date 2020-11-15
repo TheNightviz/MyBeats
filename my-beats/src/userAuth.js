045ca07512688fce61b6f9f272e97dec9c16adea
Simon Ibssa <ibssasimon@gmail.com>
2020-11-14 20:30:39 -0800

    successfully getting user playlists

--- my-beats/src/userAuth.js
+++ my-beats/src/userAuth.js
@@ -9,7 +9,26 @@
 */
 
  
-var express = require('express'); // Express web server framework
-var request = require('request'); // "Request" librarry
+var request = require("request"); // "Request" librarry
 var user_id = "ibssasimon";
-var token = "Bearer BQC2xiYZfhbRIE59gFDzdKvMzLlZNSVs_uihwjzW83ZnDI4U-N4isAO_R007_hIxBAqFb9sQKkunZtqcEIftsjpLf3VohFhjBywKZKAqMqTp17QJGUTfefBfm6VeLV1DI9iWdzGE_U6lNK8YBQ";
+var token = "Bearer BQAYMEDeOBYRbZgifhl4sbmEQamoWKp4yHDkf-h14-mZs4I_MilDyq31vGk1jBbzdt4C-DlclfnZM4Hfkmd2tjCbOcAivxlgr5-yGcN11o-QattTV6Kt3pSB2fK09TxDNiT1VkqpCNxjAlAZlA";
+
+
+var playlists_url = "https://api.spotify.com/v1/users/"+user_id+"/playlists";
+
+request({url:playlists_url, headers:{"Authorization":token}}, function(err, res){
+	if (res){
+		var playlists=JSON.parse(res.body);	
+		var playlist_url = playlists.items[0].href;
+		request({url:playlist_url, headers:{"Authorization":token}}, function(err, res){
+			if (res){
+				var playlist = JSON.parse(res.body);
+				console.log("playlist: " + playlist.name);
+				playlist.tracks.items.forEach(function(track){
+					console.log(track.track.name);
+				});
+			}
+		})		
+	}
+})
+
