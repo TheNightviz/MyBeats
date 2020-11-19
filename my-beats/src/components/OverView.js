1957a9aab7dc6eda5936b04dadfd336119aac38d
Azufire <ssarain@calpoly.edu>
2020-11-18 21:27:08 -0800

    added history and overview files

---
+++ my-beats/src/components/OverView.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import UserNav from './UserNav';
+import BottomFooter from './BottomFooter';
+
+
+const Overview = () =>
+{
+       return( <div class='pageContainer'>
+           <UserNav />
+
+           <h1>test</h1>
+           <BottomFooter />
+       </div>)
+
+}
+
+export default Overview;
