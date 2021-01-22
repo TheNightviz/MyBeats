18c09d500b8ab55cfd2eafe55073f4ba2c68ace0
TheNightviz <parkerdcallison@gmail.com>
2021-01-21 18:27:25 -0800

    Update to MyData
    Includes Bar graph with static data, css styling, and top bar information

--- my-beats/src/components/MyData.js
+++ my-beats/src/components/MyData.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import UserNav from './UserNav';
 import BottomFooter from './BottomFooter';
+import MyDataText from './MyDataText';
+import { Bar } from "react-chartjs-2";
+
+  const data = {
+    labels: ["Rock", "Blues", "Punk", "Funk", "House", "Electronic", "Other"],
+    datasets: [
+      {
+        label: "Genres",
+        data: [65, 42, 38, 32, 28, 15, 5],
+        fill: true,
+        backgroundColor: "#f2C75C",
+        borderColor: "rgba(33,33,33, 1)"
+      }
+    ]
+  };
 
 const MyData = () =>
 {
        return(
        <div class = 'pageContainer'>
            <UserNav />
-           <h1>This is MyData</h1>
-
-
+           <MyDataText />
+           <Bar 
+              data={data}
+            />
            <BottomFooter />
        </div>)
-
 }
 
 export default MyData;
