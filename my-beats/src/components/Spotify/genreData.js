21771fa616b9681e820f22bf56419c55cffecb81
Azufire <ssarain@calpoly.edu>
2021-01-29 11:34:37 -0800

    Create genre data function

--- my-beats/src/components/Spotify/genreData.js
+++ my-beats/src/components/Spotify/genreData.js
@@ -1,8 +1,13 @@
-export var pullGenreData = function(){
+//function to pull genre data from seed reccomendations
+export const pullGenreData = function() {
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token;
-    fetch('https://api.spotify.com/v1/me/top/tracks',{
-        headers: {'Authorization' : 'Bearer ' + accessToken}
-    }).then(response=> response.json()) 
-    .then(data=> console.log(data));
+        // API endpoint
+
+        fetch("https://api.spotify.com/v1/recommendations", {
+            headers: {'Authorization': 'Bearer ' + accessToken}
+        }).then(response => response.json()).then((data) => {
+            console.log(data);
+            return data;
+});
 }
