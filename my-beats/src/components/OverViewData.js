e4676231d9f07628c7c6e3bf360b58d752bd8275
TheNightviz <parkerdcallison@gmail.com>
2021-01-31 20:17:39 -0800

    Update OverViewData.js

--- my-beats/src/components/OverViewData.js
+++ my-beats/src/components/OverViewData.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { userAccessToken } from './OverView';
+import MyDataText from './MyDataText';
+import { Bar } from "react-chartjs-2";
 
 const OverViewData = () => {
     return (
        <div>
            <h1> This is MyOverViewData </h1>
            <GetTopArtists />
+           <MyDataText />
+           <Bar 
+              data={graphData}
+            />
        </div>
     );
 }
+
+const graphData = {
+    labels: ["test"],
+    datasets: [
+      {
+        label: "Top Artists",
+        data: [65, 42, 38, 32, 28, 15, 5],
+        fill: true,
+        backgroundColor: "#f2C75C",
+        borderColor: "rgba(33,33,33, 1)"
+      }
+    ]
+  };
 
 //Gets users top artists
 function GetTopArtists() {
@@ -26,7 +45,7 @@ function GetTopArtists() {
     return (
        <div>
           <h1> Data printed to console </h1>
-       </div>
+       </div> 
     )
 }
 
