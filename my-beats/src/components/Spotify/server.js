01b491547899201ca8ed1943a9463ca85592cdf7
TheNightviz <parkerdcallison@gmail.com>
2021-03-09 18:59:20 -0800

    Removed localStorage from Server

--- my-beats/src/components/Spotify/server.js
+++ my-beats/src/components/Spotify/server.js
@@ -37,7 +37,6 @@ app.get('/callback', function(req, res) {
     json: true
   }
   request.post(authOptions, function(error, response, body) {
-    localStorage.setItem('firstLogin', true);
     var access_token = body.access_token
     let uri = process.env.FRONTEND_URI || 'http://localhost:3000/OverView'
     res.redirect(uri + '?access_token=' + access_token)
